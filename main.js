0d7e2192503568fa0a6161170b03cd5adf578fd5
Aziz <19191@.ru>
2023-03-08 18:11:23 +0500

    save

---
+++ main.js
@@ -0,0 +1,24 @@
+var myName = prompt('Введите своё имя.');
+
+console.log('Меня зовут ' + myName);
+
+var myAge = +prompt('Введите вашу возраст')
+
+console.log('Мне ' + myAge);
+
+var firstExercise = +prompt('Решите пример: 6 + 3 = ?')
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + firstExercise);
+
+var secondExercise = +prompt('Решите пример: 20 - 5 = ?')
+console.log('Пример 2: 20 - 5 = 15 Ваш ответ ' + secondExercise);
+
+var thirdExercise = +prompt('Решите пример: 15 * 2 = ?')
+console.log('Пример 3: 15 * 2 = 30 Ваш ответ ' + thirdExercise);
+
+var fourthExercise = +prompt('Решите пример: 8 / 4 = ?')
+console.log('Пример 4: 8 / 4 = 2 Ваш ответ ' + fourthExercise);
+
+var fivethExercise = +prompt('Решите пример: 10 % 3 = ?')
+console.log('Пример 5: 10 % 3 Ваш ответ ' + fivethExercise);
+
+alert('Откройте консоль для просмотра')
